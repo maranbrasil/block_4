55ca743add8fab34f7b888feb17150bf8efc0dbd
Maran <maranmorimoto@gmail.com>
2021-03-24 23:14:10 -0300

    exercises 6, 7, 8, 9, 10 are done.

--- 4.1/script.js
+++ 4.1/script.js
@@ -42,9 +42,10 @@ if (a>0) {
 }
 
 /* Exercise 5 */
-var a1 = 120;
-var a2 = 50;
-var a3 = 10;
+
+let a1 = 120;
+let a2 = 50;
+let a3 = 10;
 
 if (a1 <= 0 || a2 <= 0 || a3 <=0){
   console.log("Invalid angle");
@@ -52,4 +53,109 @@ if (a1 <= 0 || a2 <= 0 || a3 <=0){
   console.log("It's a triangle!");
 } else {
   console.log("It isn't a triangle!");
+}
+
+/* Exercise 6 */
+
+let chessPiece = "King";
+let chessPieceLower = chessPiece.toLowerCase();
+
+switch (chessPieceLower) {
+  case 'bishop':
+    console.log('bishop -> diagonal');
+    break;
+  case 'king':
+    console.log('king -> vertically/horizontally');
+    break;
+  case 'rook':
+    console.log('rook -> 1 square, any direction');
+    break;
+  case 'queen':
+    console.log('queen -> any direction');
+    break;
+  case 'pawn':
+    console.log('pawn -> 1 square, diagonal');
+    break;
+    case 'knight':
+      console.log('knight -> L movement');
+      break;
+  default:
+    console.log('invalid chess piece');
+}
+
+/* Exercise 7 */
+let grade2 = 65;
+
+if (grade2 < 0 || grade2 >100) {
+  console.log('Erro!')
+} else if (grade2 > 90) {
+  console.log('A');
+} else if (grade2 > 80) {
+  console.log('B');
+} else if (grade2 > 70) {
+  console.log('C');
+} else if (grade2 > 60) {
+  console.log('D');
+} else if (grade2 > 50) {
+  console.log('E');
+} else {
+  console.log('F');
+}
+
+/* Exercise 8 */
+let ex8_1 = 7;
+let ex8_2 = 5;
+let ex8_3 = 3;
+
+if (ex8_1 % 2 === 0 || ex8_2 % 2 === 0 || ex8_3 % 2 === 0) {
+  console.log('There is one or more than a even number');
+} else {
+  console.log('There is no even number');
+}
+
+/* Exercise 9 */
+let ex9_1 = 7;
+let ex9_2 = 5;
+let ex9_3 = 3;
+
+if (ex9_1 % 2 === 1 || ex9_2 % 2 === 1 || ex8_9 % 2 === 1) {
+  console.log('True');
+} else {
+  console.log('False');
+}
+
+/* Exercise 10 */
+let prodValue = 10;
+let sellValue = 200;
+
+if (prodValue < 0 || sellValue < 0) {
+  console.log('Please, enter with a valid value');
+} else {
+  console.log((sellValue - prodValue - (0.2 * prodValue)) * 1000);
+}
+
+/* Exercise 11 */
+let income = 2794;
+let baseIncome = ;
+
+/* INSS */
+if (income < 1556,94) {
+  income * 0.92 = baseIncome;
+  console.log(income * 0.92)
+  } else if (income < 2594,92) {
+    income * 0.91 = baseIncome;
+    console.log(income * 0.91);
+    } else if (income < 5189,82) {
+      income * 0.89 = baseIncome;
+      console.log(income * 0.89);   
+      } else {
+        income * 0.89 = baseIncome;
+        console.log(income - 570,88);
+      }
+
+If (baseIncome < 1903,98) {
+  console.log(baseIncome);
+} else if (baseIncome <2826,64) {
+    console.log(baseIncome * 0.925);
+
 }
